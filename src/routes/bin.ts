b955089d0c730d8e1967a2080f6873bf4fac19ae
Mehmet FATIH <42905665+mehfatih@users.noreply.github.com>
2026-04-11 00:20:47 +0300

    Update bin.ts

--- src/routes/bin.ts
+++ src/routes/bin.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { authenticateToken } from '../middleware/auth';
 import { lookupBin, getLookupHistory, getBinStats, addBinRecord, get3dsDecision, getCountry3dsRules } from '../controllers/binController';
 import { AuthenticatedRequest } from '../types';
@@ -6,11 +6,11 @@ import { AuthenticatedRequest } from '../types';
 const router = Router();
 router.use(authenticateToken);
 
-router.post('/lookup',           (req: Request, res: Response, next: NextFunction) => lookupBin(req as AuthenticatedRequest, res, next));
-router.get('/history',           (req: Request, res: Response, next: NextFunction) => getLookupHistory(req as AuthenticatedRequest, res, next));
-router.get('/stats',             (req: Request, res: Response, next: NextFunction) => getBinStats(req as AuthenticatedRequest, res, next));
-router.post('/records',          (req: Request, res: Response, next: NextFunction) => addBinRecord(req as AuthenticatedRequest, res, next));
-router.post('/3ds-decision',     (req: Request, res: Response, next: NextFunction) => get3dsDecision(req as AuthenticatedRequest, res, next));
-router.get('/country-3ds-rules', (req: Request, res: Response, next: NextFunction) => getCountry3dsRules(req as AuthenticatedRequest, res, next));
+router.post('/lookup',           (req: Request, res: Response) => lookupBin(req as AuthenticatedRequest, res));
+router.get('/history',           (req: Request, res: Response) => getLookupHistory(req as AuthenticatedRequest, res));
+router.get('/stats',             (req: Request, res: Response) => getBinStats(req as AuthenticatedRequest, res));
+router.post('/records',          (req: Request, res: Response) => addBinRecord(req as AuthenticatedRequest, res));
+router.post('/3ds-decision',     (req: Request, res: Response) => get3dsDecision(req as AuthenticatedRequest, res));
+router.get('/country-3ds-rules', (req: Request, res: Response) => getCountry3dsRules(req as AuthenticatedRequest, res));
 
 export default router;
