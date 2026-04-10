f615fa2a182e90e1e5280ea10d8bd20d1196e95e
Mehmet FATIH <42905665+mehfatih@users.noreply.github.com>
2026-04-11 00:21:14 +0300

    Update chargeback.ts

--- src/routes/chargeback.ts
+++ src/routes/chargeback.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { authenticateToken } from '../middleware/auth';
 import { listAlerts, analyzeForChargeback, resolveAlert, listRules, createRule, updateRule, deleteRule, getStats } from '../controllers/chargebackController';
 import { AuthenticatedRequest } from '../types';
@@ -6,13 +6,13 @@ import { AuthenticatedRequest } from '../types';
 const router = Router();
 router.use(authenticateToken);
 
-router.get('/alerts',               (req: Request, res: Response, next: NextFunction) => listAlerts(req as AuthenticatedRequest, res, next));
-router.post('/analyze',             (req: Request, res: Response, next: NextFunction) => analyzeForChargeback(req as AuthenticatedRequest, res, next));
-router.patch('/alerts/:id/resolve', (req: Request, res: Response, next: NextFunction) => resolveAlert(req as AuthenticatedRequest, res, next));
-router.get('/rules',                (req: Request, res: Response, next: NextFunction) => listRules(req as AuthenticatedRequest, res, next));
-router.post('/rules',               (req: Request, res: Response, next: NextFunction) => createRule(req as AuthenticatedRequest, res, next));
-router.patch('/rules/:id',          (req: Request, res: Response, next: NextFunction) => updateRule(req as AuthenticatedRequest, res, next));
-router.delete('/rules/:id',         (req: Request, res: Response, next: NextFunction) => deleteRule(req as AuthenticatedRequest, res, next));
-router.get('/stats',                (req: Request, res: Response, next: NextFunction) => getStats(req as AuthenticatedRequest, res, next));
+router.get('/alerts',               (req: Request, res: Response) => listAlerts(req as AuthenticatedRequest, res));
+router.post('/analyze',             (req: Request, res: Response) => analyzeForChargeback(req as AuthenticatedRequest, res));
+router.patch('/alerts/:id/resolve', (req: Request, res: Response) => resolveAlert(req as AuthenticatedRequest, res));
+router.get('/rules',                (req: Request, res: Response) => listRules(req as AuthenticatedRequest, res));
+router.post('/rules',               (req: Request, res: Response) => createRule(req as AuthenticatedRequest, res));
+router.patch('/rules/:id',          (req: Request, res: Response) => updateRule(req as AuthenticatedRequest, res));
+router.delete('/rules/:id',         (req: Request, res: Response) => deleteRule(req as AuthenticatedRequest, res));
+router.get('/stats',                (req: Request, res: Response) => getStats(req as AuthenticatedRequest, res));
 
 export default router;
