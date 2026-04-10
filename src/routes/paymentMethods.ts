10f008e3d6f6610518d3ac8a88babf960fe5fed5
Mehmet FATIH <42905665+mehfatih@users.noreply.github.com>
2026-04-11 00:24:10 +0300

    Update paymentMethods.ts

--- src/routes/paymentMethods.ts
+++ src/routes/paymentMethods.ts
@@ -1,18 +1,18 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { authenticateToken } from '../middleware/auth';
 import { listMethods, updateMethod, toggleMethod, getMethodsPublic, getSuccessRates, getCountryRecommendations, getLocalizationMap } from '../controllers/paymentMethodsController';
 import { AuthenticatedRequest } from '../types';
 
 const router = Router();
 
-router.get('/public/:merchantId', (req: Request, res: Response, next: NextFunction) => getMethodsPublic(req as AuthenticatedRequest, res, next));
+router.get('/public/:merchantId', (req: Request, res: Response) => getMethodsPublic(req as AuthenticatedRequest, res));
 
 router.use(authenticateToken);
-router.get('/',                                 (req: Request, res: Response, next: NextFunction) => listMethods(req as AuthenticatedRequest, res, next));
-router.patch('/:method',                        (req: Request, res: Response, next: NextFunction) => updateMethod(req as AuthenticatedRequest, res, next));
-router.patch('/:method/toggle',                 (req: Request, res: Response, next: NextFunction) => toggleMethod(req as AuthenticatedRequest, res, next));
-router.get('/success-rates',                    (req: Request, res: Response, next: NextFunction) => getSuccessRates(req as AuthenticatedRequest, res, next));
-router.get('/localization-map',                 (req: Request, res: Response, next: NextFunction) => getLocalizationMap(req as AuthenticatedRequest, res, next));
-router.get('/country-recommendations/:country', (req: Request, res: Response, next: NextFunction) => getCountryRecommendations(req as AuthenticatedRequest, res, next));
+router.get('/',                                 (req: Request, res: Response) => listMethods(req as AuthenticatedRequest, res));
+router.patch('/:method',                        (req: Request, res: Response) => updateMethod(req as AuthenticatedRequest, res));
+router.patch('/:method/toggle',                 (req: Request, res: Response) => toggleMethod(req as AuthenticatedRequest, res));
+router.get('/success-rates',                    (req: Request, res: Response) => getSuccessRates(req as AuthenticatedRequest, res));
+router.get('/localization-map',                 (req: Request, res: Response) => getLocalizationMap(req as AuthenticatedRequest, res));
+router.get('/country-recommendations/:country', (req: Request, res: Response) => getCountryRecommendations(req as AuthenticatedRequest, res));
 
 export default router;
