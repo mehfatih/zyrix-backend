8d781a584360088043ecef6c0885af0169ebf4f1
Mehmet FATIH <42905665+mehfatih@users.noreply.github.com>
2026-04-11 00:22:11 +0300

    Update dynamicCheckout.ts

--- src/routes/dynamicCheckout.ts
+++ src/routes/dynamicCheckout.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { authenticateToken } from '../middleware/auth';
 import { listCheckouts, createCheckout, getCheckout, updateCheckout, deleteCheckout, createRule, updateRule, deleteRule, resolveCheckout, getAnalytics, personalizeCheckout, getCustomerPreferences } from '../controllers/dynamicCheckoutController';
 import { AuthenticatedRequest } from '../types';
@@ -6,17 +6,17 @@ import { AuthenticatedRequest } from '../types';
 const router = Router();
 router.use(authenticateToken);
 
-router.get('/',                               (req: Request, res: Response, next: NextFunction) => listCheckouts(req as AuthenticatedRequest, res, next));
-router.post('/',                              (req: Request, res: Response, next: NextFunction) => createCheckout(req as AuthenticatedRequest, res, next));
-router.get('/:id',                            (req: Request, res: Response, next: NextFunction) => getCheckout(req as AuthenticatedRequest, res, next));
-router.patch('/:id',                          (req: Request, res: Response, next: NextFunction) => updateCheckout(req as AuthenticatedRequest, res, next));
-router.delete('/:id',                         (req: Request, res: Response, next: NextFunction) => deleteCheckout(req as AuthenticatedRequest, res, next));
-router.post('/:id/rules',                     (req: Request, res: Response, next: NextFunction) => createRule(req as AuthenticatedRequest, res, next));
-router.patch('/:id/rules/:ruleId',            (req: Request, res: Response, next: NextFunction) => updateRule(req as AuthenticatedRequest, res, next));
-router.delete('/:id/rules/:ruleId',           (req: Request, res: Response, next: NextFunction) => deleteRule(req as AuthenticatedRequest, res, next));
-router.post('/:id/resolve',                   (req: Request, res: Response, next: NextFunction) => resolveCheckout(req as AuthenticatedRequest, res, next));
-router.get('/:id/analytics',                  (req: Request, res: Response, next: NextFunction) => getAnalytics(req as AuthenticatedRequest, res, next));
-router.post('/:id/personalize',               (req: Request, res: Response, next: NextFunction) => personalizeCheckout(req as AuthenticatedRequest, res, next));
-router.get('/:id/preferences/:customerPhone', (req: Request, res: Response, next: NextFunction) => getCustomerPreferences(req as AuthenticatedRequest, res, next));
+router.get('/',                               (req: Request, res: Response) => listCheckouts(req as AuthenticatedRequest, res));
+router.post('/',                              (req: Request, res: Response) => createCheckout(req as AuthenticatedRequest, res));
+router.get('/:id',                            (req: Request, res: Response) => getCheckout(req as AuthenticatedRequest, res));
+router.patch('/:id',                          (req: Request, res: Response) => updateCheckout(req as AuthenticatedRequest, res));
+router.delete('/:id',                         (req: Request, res: Response) => deleteCheckout(req as AuthenticatedRequest, res));
+router.post('/:id/rules',                     (req: Request, res: Response) => createRule(req as AuthenticatedRequest, res));
+router.patch('/:id/rules/:ruleId',            (req: Request, res: Response) => updateRule(req as AuthenticatedRequest, res));
+router.delete('/:id/rules/:ruleId',           (req: Request, res: Response) => deleteRule(req as AuthenticatedRequest, res));
+router.post('/:id/resolve',                   (req: Request, res: Response) => resolveCheckout(req as AuthenticatedRequest, res));
+router.get('/:id/analytics',                  (req: Request, res: Response) => getAnalytics(req as AuthenticatedRequest, res));
+router.post('/:id/personalize',               (req: Request, res: Response) => personalizeCheckout(req as AuthenticatedRequest, res));
+router.get('/:id/preferences/:customerPhone', (req: Request, res: Response) => getCustomerPreferences(req as AuthenticatedRequest, res));
 
 export default router;
