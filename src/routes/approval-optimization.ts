648062bc0024148c5d4feaeb033d0853c9501647
Mehmet FATIH <42905665+mehfatih@users.noreply.github.com>
2026-04-11 00:20:23 +0300

    Update approval-optimization.ts

--- src/routes/approval-optimization.ts
+++ src/routes/approval-optimization.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response } from 'express';
 import { authenticateToken } from '../middleware/auth';
 import { getConfig, updateConfig, analyzeApproval, updateEvent, getStats, listSla, upsertSla, checkSla } from '../controllers/approvalOptimizationController';
 import { AuthenticatedRequest } from '../types';
@@ -6,13 +6,13 @@ import { AuthenticatedRequest } from '../types';
 const router = Router();
 router.use(authenticateToken);
 
-router.get('/config',                (req: Request, res: Response, next: NextFunction) => getConfig(req as AuthenticatedRequest, res, next));
-router.patch('/config',              (req: Request, res: Response, next: NextFunction) => updateConfig(req as AuthenticatedRequest, res, next));
-router.post('/analyze',              (req: Request, res: Response, next: NextFunction) => analyzeApproval(req as AuthenticatedRequest, res, next));
-router.patch('/events/:id',          (req: Request, res: Response, next: NextFunction) => updateEvent(req as AuthenticatedRequest, res, next));
-router.get('/stats',                 (req: Request, res: Response, next: NextFunction) => getStats(req as AuthenticatedRequest, res, next));
-router.get('/sla',                   (req: Request, res: Response, next: NextFunction) => listSla(req as AuthenticatedRequest, res, next));
-router.post('/sla',                  (req: Request, res: Response, next: NextFunction) => upsertSla(req as AuthenticatedRequest, res, next));
-router.post('/sla/:gatewayId/check', (req: Request, res: Response, next: NextFunction) => checkSla(req as AuthenticatedRequest, res, next));
+router.get('/config',                (req: Request, res: Response) => getConfig(req as AuthenticatedRequest, res));
+router.patch('/config',              (req: Request, res: Response) => updateConfig(req as AuthenticatedRequest, res));
+router.post('/analyze',              (req: Request, res: Response) => analyzeApproval(req as AuthenticatedRequest, res));
+router.patch('/events/:id',          (req: Request, res: Response) => updateEvent(req as AuthenticatedRequest, res));
+router.get('/stats',                 (req: Request, res: Response) => getStats(req as AuthenticatedRequest, res));
+router.get('/sla',                   (req: Request, res: Response) => listSla(req as AuthenticatedRequest, res));
+router.post('/sla',                  (req: Request, res: Response) => upsertSla(req as AuthenticatedRequest, res));
+router.post('/sla/:gatewayId/check', (req: Request, res: Response) => checkSla(req as AuthenticatedRequest, res));
 
 export default router;
